5874eb7ea9226e4b7da3b53244a59e9e01cab74c
Halisson José Soares da Rocha <hj_rocha@id.uff.br>
2021-02-14 10:56:46 -0300

    Ajustes

--- src/app/transacoes-comerciais/compras/compras-veiculos-list/compras-veiculos-list.component.ts
+++ src/app/transacoes-comerciais/compras/compras-veiculos-list/compras-veiculos-list.component.ts
@@ -1,3 +1,6 @@
+import { Compra } from './../models/compra';
+import { Veiculo } from './../../../veiculos/models/veiculo';
+import { ComprasService } from './../services/compras.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComprasVeiculosListComponent implements OnInit {
 
-  constructor() { }
+  lista: Compra[];
+
+  itemSelecionado: Compra;
+
+  mensagemSucesso: string;
+  mensagemErro: string;
+  success: boolean;
+  errors: String[];
+
+  constructor(private service: ComprasService) { }
 
   ngOnInit(): void {
+    this.service
+      .listar()
+      .subscribe(response => {
+        this.success = true;
+        this.errors = null;
+        this.lista = response;
+      }, errorResponse => {
+        this.success = false;
+        this.mensagemErro = errorResponse.error.message;
+        this.errors = errorResponse.error.errors;
+      });
+
+  }
+
+
+  preparaDelecao(compra: Compra){
+    this.itemSelecionado = compra;
+  }
+
+  deletar(){
+    this.service
+      .deletar(this.itemSelecionado)
+      .subscribe(
+        response => {
+          this.success = true;
+          this.errors = null;
+          this.mensagemSucesso = 'Compra apagada com sucesso!'
+          this.ngOnInit();
+        },
+        errorResponse => {
+          this.success = false;
+          this.mensagemErro = errorResponse.error.message;
+          this.mensagemErro = 'Ocorreu um erro ao apagar a compra.'
+        }
+      )
   }
 
 }
